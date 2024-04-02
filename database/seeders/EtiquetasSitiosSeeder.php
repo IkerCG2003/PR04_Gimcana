b1f91b604c116c37c07ca4de7c107f6f1550c1cc
Darckfer <juliocesarcarrillorocha@gmail.com>
2024-04-02 18:07:20 +0200

    Correcion nombre seeder

--- database/seeders/EtiquetasSitiosSeeder.php
+++ database/seeders/EtiquetasSitiosSeeder.php
@@ -10,6 +10,17 @@ class EtiquetasSitiosSeeder extends Seeder
 {
     public function run(): void
     {
-
+        DB::table('tbl_etiquetas_sitios')->insert([
+            'id_etiqueta' => 1,
+            'id_sitio' => 1,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('tbl_etiquetas_sitios')->insert([
+            'id_etiqueta' => 2,
+            'id_sitio' => 1,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 }
