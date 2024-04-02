0185c8ce02f6a9d58aacd7dc2e41c8796627cf95
Joan <100007464.joan23@fje.edu>
2024-04-02 17:47:04 +0200

    subida4

--- database/seeders/SitiosSeeder.php
+++ database/seeders/SitiosSeeder.php
@@ -8,14 +8,39 @@ use Illuminate\Support\Facades\DB;
 
 class SitiosSeeder extends Seeder
 {
-    DB::table('tbl_usuarios')->insert([
-        'id' => 1,
-        'nom_usuario' => 'Adrian',
-        'apell_usuario' => 'Vazquez Pascuas',
-        'email_usuario' => 'adrianvazquez@gmail.com',
-        'pwd_usuario' => bcrypt('qweQWE123'),
-        'rol_usr' => 0, /* ROL DE ADMIN */
-        'created_at' => now(),
-        'updated_at' => now()
-    ]);
+    public function run(): void
+    {
+        DB::table('tbl_sitios')->insert([
+            'id' => 1,
+            'nom_sitio' => 'Sitio1',
+            'ubi_sitio' => '41.379028624403865, 2.126041492842137',
+            'ico_sitio' => 'sitio1',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('tbl_sitios')->insert([
+            'id' => 2,
+            'nom_sitio' => 'Sitio2',
+            'ubi_sitio' => '41.379028624403865, 2.126041492842137',
+            'ico_sitio' => 'sitio2',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('tbl_sitios')->insert([
+            'id' => 3,
+            'nom_sitio' => 'Sitio3',
+            'ubi_sitio' => '41.379028624403865, 2.126041492842137',
+            'ico_sitio' => 'sitio3',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('tbl_sitios')->insert([
+            'id' => 4,
+            'nom_sitio' => 'Sitio4',
+            'ubi_sitio' => '41.379028624403865, 2.126041492842137',
+            'ico_sitio' => 'sitio4',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+    }
 }
