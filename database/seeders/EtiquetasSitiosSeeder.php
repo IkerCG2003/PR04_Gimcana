72bbab0d17eb1457689f6f5dbcae042e5083397d
Joan <100007464.joan23@fje.edu>
2024-04-02 17:35:14 +0200

    subida4

--- database/seeders/EtiquetasSitiosSeeder.php
+++ database/seeders/EtiquetasSitiosSeeder.php
@@ -4,14 +4,65 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class EtiquetasSitiosSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        DB::table('tbl_etiquetas')->insert([
+            'id' => 1,
+            'nom_etiqueta' => 'Monumento',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('tbl_etiquetas')->insert([
+            'id' => 2,
+            'nom_etiqueta' => 'Comida',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('tbl_etiquetas')->insert([
+            'id' => 3,
+            'nom_etiqueta' => 'Parque',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('tbl_etiquetas')->insert([
+            'id' => 4,
+            'nom_etiqueta' => 'Transporte Publico',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('tbl_etiquetas')->insert([
+            'id' => 5,
+            'nom_etiqueta' => 'Hotel',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('tbl_etiquetas')->insert([
+            'id' => 6,
+            'nom_etiqueta' => 'Museo',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('tbl_etiquetas')->insert([
+            'id' => 7,
+            'nom_etiqueta' => 'Banco',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('tbl_etiquetas')->insert([
+            'id' => 8,
+            'nom_etiqueta' => 'Catedral',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('tbl_etiquetas')->insert([
+            'id' => 9,
+            'nom_etiqueta' => 'Discoteca',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 }
--- database/seeders/SitiosSeeder.php
+++ database/seeders/SitiosSeeder.php
@@ -4,14 +4,18 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SitiosSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
-    }
+    DB::table('tbl_usuarios')->insert([
+        'id' => 1,
+        'nom_usuario' => 'Adrian',
+        'apell_usuario' => 'Vazquez Pascuas',
+        'email_usuario' => 'adrianvazquez@gmail.com',
+        'pwd_usuario' => bcrypt('qweQWE123'),
+        'rol_usr' => 0, /* ROL DE ADMIN */
+        'created_at' => now(),
+        'updated_at' => now()
+    ]);
 }
