7a157937f658ba0379df95c6a53e030eaa3360ec
Joan <100007464.joan23@fje.edu>
2024-04-03 17:57:58 +0200

    subida8

--- resources/views/mapa.blade.php
+++ resources/views/mapa.blade.php
@@ -2,15 +2,12 @@
 @section('title', 'Gincana')
 @section('content')
 
-    <div id="paginamapa">
+    {{-- <div id="paginamapa"> --}}
 
         <div id="map"></div>
 
-    </div>
+    {{-- </div> --}}
 
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
-    <script src="https://unpkg.com/leaflet-routing-machine/dist/leaflet-routing-machine.js"></script>
-    <script src="{{ asset('/js/script.js') }}"></script>
+    
 
 @endsection
