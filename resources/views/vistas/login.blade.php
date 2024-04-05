61b5be457c450cd19d2787b55da6fe1dfe5cd6d9
iker catalan gomez <100007043.joan23@fje.edu>
2024-04-05 16:15:15 +0200

    Alternan contraseña tipo password y text

--- resources/views/vistas/login.blade.php
+++ resources/views/vistas/login.blade.php
@@ -4,12 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- Fontawseome --}}
+    <script src="https://kit.fontawesome.com/8e6d3dccce.js" crossorigin="anonymous"></script>    
     {{-- Bootstrap --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     {{-- Estilos --}}
     <link rel="stylesheet" href="{{asset ('css/login.css')}}">
-    {{--Logo --}}
+    {{-- Logo --}}
     <link rel="shortcut icon" href="{{asset ('img/logo.png')}}">
+    {{-- Font Awesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-odKNlp6EOVHQdWDASv/mcKo2eApZEAdWqCXf9ORJ16pM2nE0ZyqVx1Nt9k5Do2X1Fd2R8xZydz3RzVbmqRkGCA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     {{-- Título --}}
     <title>Login | Jincana</title>
 </head>
@@ -40,7 +44,12 @@
                     
                     <div class="form-group">
                         <label for="password">Contraseña:</label>
-                        <input type="password" name="password" id="password" class="form-control @error('password') is-invalid @enderror" placeholder="Ingresa tu contraseña">
+                        <div class="input-group">
+                            <input type="password" name="password" id="password" class="form-control @error('password') is-invalid @enderror" placeholder="Ingresa tu contraseña">
+                            <button type="button" id="password-toggle-btn" class="btn btn-outline-secondary" onclick="togglePasswordVisibility()">
+                                <i class="far fa-eye"></i> <!-- Ícono del ojo abierto -->
+                            </button>
+                        </div>
                         @error('password')
                             <p class="invalid-feedback">{{ $message }}</p>
                         @enderror
@@ -53,5 +62,22 @@
             </div>
         </div>
     </div>
+
+    <!-- Scripts JavaScript -->
+    <script>
+        // Función para alternar la visibilidad de la contraseña
+        function togglePasswordVisibility() {
+            var passwordInput = document.getElementById("password");
+            var passwordToggleBtn = document.getElementById("password-toggle-btn");
+
+            if (passwordInput.type === "password") {
+                passwordInput.type = "text";
+                passwordToggleBtn.innerHTML = '<i class="far fa-eye-slash"></i>'; // Cambiar ícono a ojo tachado
+            } else {
+                passwordInput.type = "password";
+                passwordToggleBtn.innerHTML = '<i class="far fa-eye"></i>'; // Cambiar ícono a ojo abierto
+            }
+        }
+    </script>
 </body>
 </html>
--- resources/views/vistas/register.blade.php
+++ resources/views/vistas/register.blade.php
@@ -1,20 +1,24 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- Font Awesome --}}
+    <script src="https://kit.fontawesome.com/8e6d3dccce.js" crossorigin="anonymous"></script>
     {{-- Bootstrap --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     {{-- Estilos --}}
-    <link rel="stylesheet" href="{{asset ('css/login.css')}}">
-    {{--Logo --}}
-    <link rel="shortcut icon" href="{{asset ('img/logo.png')}}">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    {{-- Logo --}}
+    <link rel="shortcut icon" href="{{ asset('img/logo.png') }}">
     {{-- Título --}}
-    <title>Login | Jincana</title>
+    <title>Registro | Jincana</title>
 </head>
 
-<body>        
+<body>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-6">
@@ -22,25 +26,25 @@
                     <h2 class="mb-4">Registrar usuario</h2>
                     @csrf
 
-                    @if(session('error'))
+                    @if (session('error'))
                         <div class="alert alert-danger">{{ session('error') }}</div>
                     @endif
-                
-                    {{-- @if(session('success'))
-                        <div class="alert alert-success">{{ session('success') }}</div>
-                    @endif         --}}
 
                     <div class="form-group">
                         <label for="nombre">Nombre:</label>
-                        <input type="nombre" name="nombre" id="nombre" class="form-control @error('nombre') is-invalid @enderror" value="{{ old('nombre') }}" placeholder="Ingresa tu nombre">
+                        <input type="nombre" name="nombre" id="nombre"
+                            class="form-control @error('nombre') is-invalid @enderror" value="{{ old('nombre') }}"
+                            placeholder="Ingresa tu nombre">
                         @error('nombre')
                             <p class="invalid-feedback">{{ $message }}</p>
                         @enderror
                     </div>
-                    
+
                     <div class="form-group">
                         <label for="apellido">Apellidos:</label>
-                        <input type="apellido" name="apellido" id="apellido" class="form-control @error('apellido') is-invalid @enderror" value="{{ old('apellido') }}" placeholder="Ingresa tus apellidos">
+                        <input type="apellido" name="apellido" id="apellido"
+                            class="form-control @error('apellido') is-invalid @enderror" value="{{ old('apellido') }}"
+                            placeholder="Ingresa tus apellidos">
                         @error('apellido')
                             <p class="invalid-feedback">{{ $message }}</p>
                         @enderror
@@ -48,34 +52,71 @@
 
                     <div class="form-group">
                         <label for="email">Correo electrónico:</label>
-                        <input type="email" name="email" id="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" placeholder="Ingresa tu email">
+                        <input type="email" name="email" id="email"
+                            class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}"
+                            placeholder="Ingresa tu email">
                         @error('email')
                             <p class="invalid-feedback">{{ $message }}</p>
                         @enderror
                     </div>
-                    
+
                     <div class="form-group">
                         <label for="password-1">Contraseña:</label>
-                        <input type="password" name="password-1" id="password-1" class="form-control @error('password-1') is-invalid @enderror" placeholder="Ingresa tu contraseña">
+                        <div class="input-group">
+                            <input type="password" name="password-1" id="password-1"
+                                class="form-control @error('password-1') is-invalid @enderror"
+                                placeholder="Ingresa tu contraseña">
+                            <button type="button" class="btn btn-outline-secondary"
+                                onclick="togglePasswordVisibility('password-1')">
+                                <i class="far fa-eye"></i>
+                            </button>
+                        </div>
                         @error('password-1')
                             <p class="invalid-feedback">{{ $message }}</p>
                         @enderror
                     </div>
-                    
+
                     <div class="form-group">
                         <label for="password-2">Repite la contraseña:</label>
-                        <input type="password" name="password-1_confirmation" id="password-2" class="form-control @error('password-2') is-invalid @enderror" placeholder="Repite tu contraseña">
+                        <div class="input-group">
+                            <input type="password" name="password-2" id="password-2"
+                                class="form-control @error('password-2') is-invalid @enderror"
+                                placeholder="Repite tu contraseña">
+                            <button type="button" class="btn btn-outline-secondary"
+                                onclick="togglePasswordVisibility('password-2')">
+                                <i class="far fa-eye"></i>
+                            </button>
+                        </div>
                         @error('password-2')
                             <p class="invalid-feedback">{{ $message }}</p>
                         @enderror
                     </div>
-                                        
+
                     <button type="submit" class="btn btn-primary">Registrar</button>
-                                        
-                    <a href="{{ route('login') }}"><p>¿Ya tienes cuenta? Inicia sesión aquí.</p></a>
-                </form> 
+
+                    <a href="{{ route('login') }}">
+                        <p>¿Ya tienes cuenta? Inicia sesión aquí.</p>
+                    </a>
+                </form>
             </div>
         </div>
     </div>
+
+    <!-- Scripts JavaScript -->
+    <script>
+        function togglePasswordVisibility(inputId) {
+            var passwordInput = document.getElementById(inputId);
+            var passwordToggleBtn = passwordInput.nextElementSibling;
+
+            if (passwordInput.type === "password") {
+                passwordInput.type = "text";
+                passwordToggleBtn.innerHTML = '<i class="far fa-eye-slash"></i>'; // Cambiar ícono a ojo tachado
+            } else {
+                passwordInput.type = "password";
+                passwordToggleBtn.innerHTML = '<i class="far fa-eye"></i>'; // Cambiar ícono a ojo abierto
+            }
+        }
+    </script>
 </body>
+
 </html>
