98359290be85cd49818d39fafd746dbb6f68406e
iker catalan gomez <100007043.joan23@fje.edu>
2024-04-12 17:33:56 +0200

    Pruebas Seeder

--- database/seeders/PruebasSeeder.php
+++ database/seeders/PruebasSeeder.php
@@ -11,36 +11,36 @@ class PruebasSeeder extends Seeder
     public function run(): void
     {
         DB::table('tbl_pruebas')->insert([
-            'nom_prueba' => 'Prueba1',
-            'pista_prueba' => 'Pista prueba 1',
+            'nom_prueba' => 'Primera prueba',
+            'pista_prueba' => 'Encuentra el lugar donde los libros son tesoros y los pupitres son barcos que navegan por mares de conocimiento. Busca una placa con el nombre de un filósofo español que da la bienvenida a los estudiantes en la entrada principal. Ahí es donde encontrarás tu siguiente pista.',
             'estado_prueba' => 1, /* ESTADO ACTIVADA */
             'created_at' => now(),
             'updated_at' => now()
         ]);
         DB::table('tbl_pruebas')->insert([
-            'nom_prueba' => 'Prueba2',
-            'pista_prueba' => 'Pista prueba 2',
-            'estado_prueba' => 1, /* ESTADO ACTIVADA */
+            'nom_prueba' => 'Segunda prueba',
+            'pista_prueba' => 'En este punto de partida y llegada local, los viajeros se cruzan en un ir y venir constante, buscando destinos lejanos o regresando a casa. Encuentra el lugar donde los trenes rugen como bestias metálicas y los pasajeros se convierten en aventureros urbanos.',
+            'estado_prueba' => 0, /* ESTADO ACTIVADA */
             'created_at' => now(),
             'updated_at' => now()
         ]);
         DB::table('tbl_pruebas')->insert([
-            'nom_prueba' => 'Prueba3',
-            'pista_prueba' => 'Pista prueba 3',
-            'estado_prueba' => 1, /* ESTADO ACTIVADA */
+            'nom_prueba' => 'Tercera prueba',
+            'pista_prueba' => 'En este lugar de encuentro cultural, las artes se entrelazan como hilos de colores en un telar, tejiendo la diversidad y la creatividad de la comunidad. Busca el espacio donde las palabras se convierten en poesía, los colores cobran vida y la música se convierte en melodía.',
+            'estado_prueba' => 0, /* ESTADO ACTIVADA */
             'created_at' => now(),
             'updated_at' => now()
         ]);
         DB::table('tbl_pruebas')->insert([
-            'nom_prueba' => 'Prueba4',
-            'pista_prueba' => 'Pista prueba 4',
-            'estado_prueba' => 1, /* ESTADO ACTIVADA */
+            'nom_prueba' => 'Cuarta prueba',
+            'pista_prueba' => 'En este lugar subterráneo, las líneas de la ciudad se entrelazan como venas que conectan el corazón de Barcelona. Busca el punto donde los trenes rugen como dragones metálicos y los pasajeros son como viajeros en una odisea urbana.',
+            'estado_prueba' => 0, /* ESTADO ACTIVADA */
             'created_at' => now(),
             'updated_at' => now()
         ]);
         DB::table('tbl_pruebas')->insert([
-            'nom_prueba' => 'Prueba5',
-            'pista_prueba' => 'Pista prueba 5',
+            'nom_prueba' => 'Quinta prueba',
+            'pista_prueba' => 'En este oasis verde en medio del bullicio urbano, una figura de bronce se alza en un equilibrio perpetuo, recordándonos la armonía entre la naturaleza y la creatividad humana. Busca el punto donde la escultura se convierte en un símbolo de estabilidad y movimiento',
             'estado_prueba' => 0, /* ESTADO DESACTIVADA */
             'created_at' => now(),
             'updated_at' => now()
